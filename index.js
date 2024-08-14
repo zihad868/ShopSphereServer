3adbaf7fc76e0e8e52f42172326836e5b402d359
zihad868 <zihadh868@gmail.com>
2024-08-15 00:41:46 +0600

    Test

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
     res.send("Shop Sphere server is running ...")
 })
 
+app.get('/test', (req, res) => {
+    res.send("testing Purpose")
+})
+
 app.listen(port, () => {
     console.log(`Shop Sphere server is running on port ${port}`)
 })
